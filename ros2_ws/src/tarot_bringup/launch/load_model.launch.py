b42c985b7dd4afb93e0919562b6ba95781754114
Machinoro <95751244+machinoro@users.noreply.github.com>
2026-01-10 15:25:32 +0700

    refactor: rename uav->tarot and packages management

---
+++ ros2_ws/src/tarot_bringup/launch/load_model.launch.py
@@ -0,0 +1,41 @@
+import os
+import xacro
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    pkg = get_package_share_directory("tarot_description")
+    pkg_ros_gz_sim = get_package_share_directory("ros_gz_sim")
+
+    urdf_path = os.path.join(pkg, "urdf", "tarot.urdf.xacro")
+
+    resource_path = SetEnvironmentVariable(
+        name="GZ_SIM_RESOURCE_PATH", value=[os.path.join(pkg, "..")]
+    )
+
+    robot_desc = xacro.process_file(urdf_path).toxml()
+
+    gazebo_sim = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_ros_gz_sim, "launch", "gz_sim.launch.py")
+        ),
+        launch_arguments={"gz_args": "-r empty.sdf"}.items(),
+    )
+
+    spawn_robot = Node(
+        package="ros_gz_sim",
+        executable="create",
+        arguments=[
+            "-name",
+            "tarot",
+            "-string",
+            robot_desc,
+        ],
+        output="screen",
+    )
+
+    return LaunchDescription([resource_path, gazebo_sim, spawn_robot])
