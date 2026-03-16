fbaaf8c76aaa3d3d2c561c6e7c907353b1806f54
Machinoro <95751244+machinoro@users.noreply.github.com>
2026-03-16 20:30:39 +0700

    fix: rewrite description files for px4

---
+++ ros2_ws/src/agent_bringup/launch/load_model.launch.py
@@ -0,0 +1,41 @@
+import os
+import xacro
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    pkg = get_package_share_directory("agent_description")
+    pkg_ros_gz_sim = get_package_share_directory("ros_gz_sim")
+
+    urdf_path = os.path.join(pkg, "urdf", "agent.urdf.xacro")
+
+    robot_desc = xacro.process_file(urdf_path).toxml()
+
+    resource_path = SetEnvironmentVariable(
+        name="GZ_SIM_RESOURCE_PATH", value=[os.path.join(pkg, "..")]
+    )
+
+    gazebo_sim = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_ros_gz_sim, "launch", "gz_sim.launch.py")
+        ),
+        launch_arguments={"gz_args": "-r empty.sdf"}.items(),
+    )
+
+    spawn_robot = Node(
+        package="ros_gz_sim",
+        executable="create",
+        arguments=[
+            "-name",
+            "agent",
+            "-string",
+            robot_desc,
+        ],
+        output="screen",
+    )
+
+    return LaunchDescription([resource_path, gazebo_sim, spawn_robot])
